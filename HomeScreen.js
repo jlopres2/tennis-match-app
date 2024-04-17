86776ad01a6f63ef310363425db63c9cd5e3c844
ellaforese <ellaforese@gmail.com>
2024-04-16 23:11:26 -0400

    added filter and display scores

--- HomeScreen.js
+++ HomeScreen.js
@@ -14,12 +14,22 @@ const HomeScreen = ({ navigation }) => {
       const response = await fetch('https://api.sportradar.com/tennis/trial/v3/en/schedules/live/summaries.json?api_key=61L6LUghNb8h1dABHxJc7602f0yMnC2a8PHFtJhW');
       const data = await response.json();
       // Extracting relevant information from the API response
-      const extractedMatches = data.summaries.map(summary => ({
+      const extractedMatches = data.summaries
+      .filter(summary => summary.sport_event_status.match_status !== null && summary.sport_event_status.match_status !== "not_started")
+      .map(summary => ({
+        matchStatus: summary.sport_event_status.match_status,
         player1: summary.sport_event.competitors[0].name,
         player2: summary.sport_event.competitors[1].name,
         competitionName: summary.sport_event.sport_event_context.competition.name,
-        competitionLevel: summary.sport_event.sport_event_context.competition.level,
-        score: summary.sport_event_status.match_status,
+       // competitionLevel: summary.sport_event.sport_event_context.competition.level,
+      
+        p1Score: summary.sport_event_status.home_score,
+        p2Score: summary.sport_event_status.away_score,
+      
+
+        // could there be a line in here that calls on a method which somehow checks is matchstatus
+        // is not null or not_started
+        // if that is the case, we don't want to show or do notifications for that match
       }));
       setMatches(extractedMatches);
     } catch (error) {
@@ -30,9 +40,9 @@ const HomeScreen = ({ navigation }) => {
   const renderItem = ({ item }) => (
     <View style={styles.matchContainer}>
       <Text style={styles.matchText}>{item.player1} vs {item.player2}</Text>
+      <Text style={styles.matchText}>Score: {item.p1Score} - {item.p2Score}</Text>
       <Text style={styles.matchText}>Competition: {item.competitionName}</Text>
-      <Text style={styles.matchText}>Level: {item.competitionLevel}</Text>
-      <Text style={styles.matchText}>Score: {item.score}</Text>
+      <Text style={styles.matchText}>Match Status: {item.matchStatus}</Text>
     </View>
   );
 
@@ -70,53 +80,3 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { StyleSheet, Text, View, Button} from 'react-native';
-// import React from 'react';
-
-// const HomeScreen = ({ navigation }) => {
-//   return (
-//     <View style={styles.container}>
-//       {/* <Button
-//         title="Home"
-//         onPress={() => navigation.navigate('Home')}
-//       /> */}
-//       <Button
-//         title="Find matches"
-//         onPress={() => navigation.navigate('Search')}
-//       />
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
-
-// export default HomeScreen;
--- NotificationScreen.js
+++ NotificationScreen.js
@@ -42,16 +42,11 @@ const NotificationScreen = ({ navigation }) => {
       style={{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-around',
+        justifyContent: 'flex-start',
+        paddingTop: 100,
       }}>
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-      </View>
       <Button
-        title="Press to schedule a notification"
+        title="Press to get match updates"
         onPress={async () => {
           await schedulePushNotification();
         }}
@@ -65,12 +60,17 @@ async function fetchMatchesFromAPI() {
   try {
     const response = await fetch('https://api.sportradar.com/tennis/trial/v3/en/schedules/live/summaries.json?api_key=61L6LUghNb8h1dABHxJc7602f0yMnC2a8PHFtJhW');
     const data = await response.json();
-    const extractedMatches = data.summaries.map(summary => ({
+    const extractedMatches = data.summaries
+    .filter(summary => summary.sport_event_status.match_status !== null && summary.sport_event_status.match_status !== "not_started")
+    .map(summary => ({
       player1: summary.sport_event.competitors[0].name,
       player2: summary.sport_event.competitors[1].name,
       competitionName: summary.sport_event.sport_event_context.competition.name,
-      gameStatus: summary.sport_event_status.status,
-      gameState: summary.sport_event_status.match_status,
+      competitionLevel: summary.sport_event.sport_event_context.competition.level,
+      matchStatus: summary.sport_event_status.match_status,
+      p1Score: summary.sport_event_status.home_score,
+      p2Score: summary.sport_event_status.away_score,
+      
     }));
     return extractedMatches;
   } catch (error) {
@@ -79,13 +79,14 @@ async function fetchMatchesFromAPI() {
   }
   }
 
-async function schedulePushNotification() {
+async function schedulePushNotification(){
   const matches = await fetchMatchesFromAPI();
     matches.forEach(async match => {
       await Notifications.scheduleNotificationAsync({
         content: {
           title: "Match Notification",
-          body: `${match.player1} vs ${match.player2} - ${match.competitionName}`,
+          body: `${match.player1} vs ${match.player2}\n ${match.p1Score} - ${match.p2Score} \n ${match.competitionName}`,
+
         },
         trigger: { seconds: 2 },
       });
@@ -104,181 +105,8 @@ async function registerForPushNotificationsAsync() {
     });
   }
 
-  // if (Device.isDevice) {
-  //   const { status: existingStatus } = await Notifications.getPermissionsAsync();
-  //   let finalStatus = existingStatus;
-  //   if (existingStatus !== 'granted') {
-  //     const { status } = await Notifications.requestPermissionsAsync();
-  //     finalStatus = status;
-  //   }
-  //   if (finalStatus !== 'granted') {
-  //     alert('Failed to get push token for push notification!');
-  //     return;
-  //   }
-  //   // Learn more about projectId:
-  //   // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-  //   token = (await Notifications.getExpoPushTokenAsync({ projectId: 'your-project-id' })).data;
-  //   console.log(token);
-  // } else {
-  //   alert('Must use physical device for Push Notifications');
-  // }
 
   return token;
 }
 
 export default NotificationScreen;
-
-
-
-
-
-
-
-
-
-// import { useState, useEffect, useRef } from 'react';
-// import { Text, View, Button, Platform } from 'react-native';
-
-// import * as Notifications from 'expo-notifications';
-
-// Notifications.setNotificationHandler({
-//   handleNotification: async () => ({
-//     shouldShowAlert: true,
-//     shouldPlaySound: false,
-//     shouldSetBadge: false,
-//   }),
-// });
-
-// export default function App() {
-//   const [expoPushToken, setExpoPushToken] = useState('');
-//   const [notification, setNotification] = useState(false);
-//   const notificationListener = useRef();
-//   const responseListener = useRef();
-
-//   useEffect(() => {
-//     registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-//     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-//       setNotification(notification);
-//     });
-
-//     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-//       console.log(response);
-//     });
-
-//     // Call function that calls API
-
-//     return () => {
-//       Notifications.removeNotificationSubscription(notificationListener.current);
-//       Notifications.removeNotificationSubscription(responseListener.current);
-//     };
-//   }, []);
-
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         alignItems: 'center',
-//         justifyContent: 'space-around',
-//       }}>
-//       <Text>Your expo push token: {expoPushToken}</Text>
-//       <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Title: {notification && notification.request.content.title} </Text>
-//         <Text>Body: {notification && notification.request.content.body}</Text>
-//         <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-//       </View>
-//       <Button
-//         title="Press to schedule a notification"
-//         onPress={async () => {
-//           await schedulePushNotification();
-//         }}
-//       />
-//     </View>
-//   );
-// }
-
-// async function schedulePushNotification() {
-//   await Notifications.scheduleNotificationAsync({
-//     content: {
-//       title: " Match Point",
-//       body: 'Here is the notification body',
-//       data: { data: 'goes here' },
-//     },
-//     trigger: { seconds: 2 },
-//   });
-// }
-
-// async function registerForPushNotificationsAsync() {
-//   let token;
-
-//   if (Platform.OS === 'android') {
-//     await Notifications.setNotificationChannelAsync('default', {
-//       name: 'default',
-//       importance: Notifications.AndroidImportance.MAX,
-//       vibrationPattern: [0, 250, 250, 250],
-//       lightColor: '#FF231F7C',
-//     });
-//   }
-
-//   // if (Device.isDevice) {
-//   //   const { status: existingStatus } = await Notifications.getPermissionsAsync();
-//   //   let finalStatus = existingStatus;
-//   //   if (existingStatus !== 'granted') {
-//   //     const { status } = await Notifications.requestPermissionsAsync();
-//   //     finalStatus = status;
-//   //   }
-//   //   if (finalStatus !== 'granted') {
-//   //     alert('Failed to get push token for push notification!');
-//   //     return;
-//   //   }
-//   //   // Learn more about projectId:
-//   //   // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-//   //   token = (await Notifications.getExpoPushTokenAsync({ projectId: 'your-project-id' })).data;
-//   //   console.log(token);
-//   // } else {
-//   //   alert('Must use physical device for Push Notifications');
-//   // }
-
-//   return token;
-// }
-
-
-
-
-
-// import React from 'react';
-// import { View, Button, StyleSheet } from 'react-native';
-
-// const SearchScreen = ({ navigation }) => {
-
-
-//   return (
-//     <View style={styles.container}>
-//       {/* <Button
-//         title="Home"
-//         onPress={() => navigation.navigate('Home')}
-//       />
-//       <Button
-//         title="Search"
-//         onPress={() => navigation.navigate('Search')}
-//       /> */}
-//     </View>
-//   );
-// };
-
-// // const options = {method: 'GET', headers: {accept: 'application/json'}};
-
-// // fetch('https://api.sportradar.com/tennis/trial/v3/en/schedules/live/summaries.json?api_key=61L6LUghNb8h1dABHxJc7602f0yMnC2a8PHFtJhW', options)
-// //   .then(response => response.json())
-// //   .then(response => console.log(response))
-// //   .catch(err => console.error(err));
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
-
-// export default SearchScreen;
